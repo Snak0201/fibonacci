2207a96e288495db8a72712018f207924cfcd835
Snak <s.nakajima@4nonome.com>
2022-12-01 11:49:10 +0900

    recieve query param

--- api/apis.py
+++ api/apis.py
@@ -1,7 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 
 class FibonacciAPIView(APIView):
+    def fibonacci(self, n:int)->int:
+        """フィボナッチ数列のn番目を返す"""
+        
+        if n == 1:
+            return n
+        
+        fib_prev = 1
+        fib = 1
+        
+        for _ in range(n-2):
+            fib_prev, fib = fib, fib+fib_prev
+        
+        return fib
+    
     def get(self, request):
-        return Response({"message":"OK"}, status=200)
+        """GETレスポンス"""
+        
+        n = int(request.query_params.get("n"))
+        fibonacci = self.fibonacci(n)
+        return Response({"result": fibonacci}, status=status.HTTP_200_OK)
