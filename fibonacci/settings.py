0ba3181d459dce514ce23cb77c7f037f73d69dc6
Snak <s.nakajima@4nonome.com>
2022-12-01 14:13:12 +0900

    fix deploy setting

--- fibonacci/settings.py
+++ fibonacci/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "566ec652-867f-44bb-897f-3bb6b664fb17"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snak.pythonanywhere.com']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
